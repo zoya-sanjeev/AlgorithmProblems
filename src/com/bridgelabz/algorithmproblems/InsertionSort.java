67e1f50c2cad769d79b9db27f980cd2033f0535e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-02 22:45:12 +0530

    REFACTORED: generic methods for insertionSort

--- src/com/bridgelabz/algorithmproblems/InsertionSort.java
+++ src/com/bridgelabz/algorithmproblems/InsertionSort.java
@@ -2,13 +2,13 @@ package com.bridgelabz.algorithmproblems;
 
 public class InsertionSort {
 	
-	static String[] array="banana orange Peache apple Grapes".split(" ");
 	
-	static void insertionSort(String[] array) {
+	
+	static<K extends Comparable<K>> void insertionSort(K[] array) {
 		
 		int length = array.length;
         for (int i = 1; i < length; ++i) {
-            String key = array[i];
+            K key = array[i];
             int j = i - 1;
             while (j >= 0 && array[j].compareTo(key)>0) {
                 array[j + 1] = array[j];
@@ -19,9 +19,9 @@ public class InsertionSort {
 		
 	}
 	
-	static void printArray(String[] array) {
+	static<K> void printArray(K[] array) {
 		
-		for(String word:array) {
+		for(K word:array) {
 			System.out.print(word+" ");
 		}
 		
@@ -29,6 +29,7 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 		
+		String[] array="banana orange Peache apple Grapes".split(" ");
 		insertionSort(array);
 		printArray(array);
 		
